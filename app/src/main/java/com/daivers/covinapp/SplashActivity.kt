573f18f54db6667740acfdb31bacfc36f0f166bb
Andiez Satria <andis.permana12@gmail.com>
2020-05-15 16:19:57 +0700

    Add Splash Screen and Navigation Graph

--- app/src/main/java/com/daivers/covinapp/SplashActivity.kt
+++ app/src/main/java/com/daivers/covinapp/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.daivers.covinapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            kotlin.run {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 1500)
     }
 }
