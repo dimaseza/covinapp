c470cc60a0fd1b6a60f6cae4227b37ef975d095d
dimaseza <dimasandre18@gmail.com>
2020-05-22 11:26:42 +0700

    Change background to light theme, adding logo covin blue, adding province logo, adding resource button get blue and white, add class utils for link province

---
+++ app/src/main/java/com/daivers/covinapp/utils/LinkProvince.kt
@@ -0,0 +1,28 @@
+package com.daivers.covinapp.utils
+
+class LinkProvince(val url: String) {}
+
+val listURL = listOf<LinkProvince>(
+    LinkProvince("https://covid19.acehprov.go.id/"), // aceh
+    LinkProvince("https://www.babelprov.go.id/"), // bangka belitung
+    LinkProvince("https://www.bantenprov.go.id/covid-19"), // banten
+    LinkProvince("https://corona.jogjaprov.go.id/"), // jogja
+    LinkProvince("https://corona.jakarta.go.id/id"), // jakarta
+    LinkProvince("http://corona.jambiprov.go.id/"), // jambi
+    LinkProvince("https://pikobar.jabarprov.go.id/"), // jabar
+    LinkProvince("https://corona.jatengprov.go.id/"), // jateng
+    LinkProvince("http://infocovid19.jatimprov.go.id/"), // jatim
+    LinkProvince("https://dinkes.kalbarprov.go.id/covid-19/"), // kalbar
+    LinkProvince("http://dinkes.kalselprov.go.id/"), // kalsel
+    LinkProvince("https://kalteng.go.id/page/106/website-kab-kota"), // kalteng
+    LinkProvince("https://kaltimprov.go.id/kategori/kesehatan"), // kaltim
+    LinkProvince("https://www.kepriprov.go.id/"), // kepulauan riau
+    LinkProvince("https://covid19.lampungprov.go.id/"), // lampung
+    LinkProvince("https://corona.ntbprov.go.id/"), // ntb
+    LinkProvince("https://corona.riau.go.id/"), // riau
+    LinkProvince("https://covid19.sulselprov.go.id/"), // sulsel
+    LinkProvince("https://dinkes.sultengprov.go.id/category/covid-19/"), // sulteng
+    LinkProvince("http://www.sultraprov.go.id/"), // sultra
+    LinkProvince("https://corona.sumbarprov.go.id/"), // sumbar
+    LinkProvince("https://sumselprov.go.id/") // sumsel
+)
