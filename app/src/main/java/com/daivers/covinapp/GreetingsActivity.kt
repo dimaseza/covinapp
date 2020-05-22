6bf01df322db5367aa69c5d07a405b967a575638
Andiez Satria <andis.permana12@gmail.com>
2020-05-22 17:08:44 +0700

    Add greetings

--- app/src/main/java/com/daivers/covinapp/GreetingsActivity.kt
+++ app/src/main/java/com/daivers/covinapp/GreetingsActivity.kt
@@ -1,12 +1,33 @@
 package com.daivers.covinapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.content.edit
+import androidx.databinding.DataBindingUtil
+import com.daivers.covinapp.databinding.ActivityGreetingsBinding
+import com.daivers.covinapp.databinding.ActivityMainBinding
 
 class GreetingsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityGreetingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_greetings)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_greetings)
+
+        val prefs = getSharedPreferences(SplashActivity.PREFS_TAG, Context.MODE_PRIVATE)
+
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            prefs.edit {
+                putBoolean(SplashActivity.FIRST_TAG, false)
+                apply()
+            }
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/daivers/covinapp/SplashActivity.kt
+++ app/src/main/java/com/daivers/covinapp/SplashActivity.kt
@@ -1,19 +1,30 @@
 package com.daivers.covinapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
+    companion object {
+        const val PREFS_TAG = "prefs"
+        const val FIRST_TAG = "firstStart"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val prefs = getSharedPreferences(PREFS_TAG, Context.MODE_PRIVATE)
+        val firstStart: Boolean = prefs.getBoolean(FIRST_TAG, true)
+
         Handler().postDelayed({
             kotlin.run {
-                val intent = Intent(this, MainActivity::class.java)
+                val intent = if (firstStart) Intent(this, GreetingsActivity::class.java)
+                else Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
             }
